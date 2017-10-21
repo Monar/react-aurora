54d489f3b161abec1cd9dce60d4d60a27b0bef88
Piotr Monarski <dev@monarski.pl>
2017-10-21 17:20:44 +0200

    v0.0.3 - first working implementation
    In this version its possible some elements in the center of the screen
    with optional darkening overlay.

---
+++ src/Aurora.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Element } from './Element';
+
+export class Aurora extends React.PureComponent {
+  static propTypes = {
+    elements: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+      }),
+    ),
+    overlayColor: PropTypes.string,
+    onClose: PropTypes.func,
+    zIndex: PropTypes.number,
+  };
+
+  static defaultProps = {
+    overlayColor: 'rgba(0,0,0,0.5)',
+    zIndex: 1000,
+  };
+
+  handleOnClose = (event, id) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.props.onClose) {
+      this.props.onClose(id);
+    }
+    return false;
+  };
+
+  createElement = (element, index) => (
+    <Element
+      key={element.id}
+      data={element}
+      overlayColor={this.props.overlayColor}
+      zIndex={this.props.zIndex + index}
+      onClose={this.handleOnClose}
+    />
+  );
+
+  render() {
+    const { elements } = this.props;
+    return elements.map(this.createElement);
+  }
+}
---
+++ src/Element.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Element extends React.PureComponent {
+  static propTypes = {
+    data: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired,
+      component: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
+        .isRequired,
+      props: PropTypes.object,
+      overlay: PropTypes.bool,
+      overlayColor: PropTypes.string,
+    }),
+    zIndex: PropTypes.number.isRequired,
+    onClose: PropTypes.func,
+    overlayColor: PropTypes.string,
+  };
+
+  getStyle = data => ({
+    position: 'fixed',
+    zIndex: this.props.zIndex,
+    top: `calc(50vh - ${data.height}px / 2)`,
+    left: `calc(50vw - ${data.width}px / 2)`,
+    width: data.width,
+    height: data.height,
+  });
+
+  handleClose = event => {
+    this.props.onClose(event, this.props.data.id);
+  };
+
+  getOverlay = () => {
+    const { data: { id, overlay, overlayColor }, zIndex } = this.props;
+    if (!overlay) return null;
+
+    return (
+      <Overlay
+        key={`overlay: ${id}`}
+        color={overlayColor || this.props.overlayColor}
+        zIndex={zIndex}
+        onClick={this.handleClose}
+      />
+    );
+  };
+
+  render() {
+    const { data } = this.props;
+    const { id, component: Component, props = {} } = data;
+    return [
+      this.getOverlay(),
+      <div key={id} style={this.getStyle(data)} onClick={blockEvent}>
+        <Component {...props} onClose={this.handleClose} />
+      </div>,
+    ];
+  }
+}
+
+const Overlay = ({ onClick, color, zIndex }) => (
+  <div
+    onClick={onClick}
+    style={{
+      backgroundColor: color,
+      position: 'fixed',
+      zIndex,
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    }}
+  />
+);
+Overlay.displayName = 'AuroraElementOverlay';
+
+function blockEvent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
